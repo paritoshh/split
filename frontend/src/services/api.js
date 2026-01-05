7d90dad47890ae1019ec162280751356738bd977
paritosh-fw <paritosh.agarwal@freshworks.com>
2026-01-05 20:31:49 +0530

    Add getAllItems import for verification

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -20,7 +20,7 @@
 import axios from 'axios'
 import { offlineDetector } from './offline/detector.js'
 import { apiCache } from './offline/cache.js'
-import { addToQueue, QUEUE_TYPE } from './offline/syncQueue.js'
+import { addToQueue, QUEUE_TYPE, getAllItems } from './offline/syncQueue.js'
 
 // Flag to prevent redirects during initial auth check
 let isCheckingAuth = false
